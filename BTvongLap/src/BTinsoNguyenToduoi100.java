1171c4d3cac415b8aa36284e7a94a16fb416056f
omgbqh98 <69282402+omgbqh98@users.noreply.github.com>
2020-09-10 17:10:30 +0700

    insonguyento100

---
+++ BTvongLap/src/BTinsoNguyenToduoi100.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class BTinsoNguyenToduoi100 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input so lương prime");
+        int number = scanner.nextInt();
+        int count = 0;
+        int N = 2;
+        boolean check = true;
+        while (count < number&&number<100) {
+            if (number < 2) {
+                check=false;
+            } else {
+                int i = 2;
+                while (i <= Math.sqrt(N)) {
+                    if (N % i == 0) {
+                        check = false;
+                        break;
+                    }else {
+                        check=true;
+                    }
+                    i++;
+                } if(check){
+                    System.out.println(N);
+                    count++;
+                }N++;
+
+            }
+
+        }
+    }
+}
