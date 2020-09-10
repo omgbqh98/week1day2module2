bbc4da30137574ec1f5234c7a35d0f0de1bd9fc0
omgbqh98 <69282402+omgbqh98@users.noreply.github.com>
2020-09-10 08:58:15 +0700

    thktprime

---
+++ TH/src/THkiemTraSoNguyenTo.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class THkiemTraSoNguyenTo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input number want check");
+        int number = scanner.nextInt();
+boolean check =true;
+        if(number<2){
+            check = false;
+        }else {
+            for (int i = 2; i <= Math.sqrt(number) ; i++) {
+                if(number%i==0){
+                    check = false;
+                    break;
+                }else {
+                    check =true;
+                    break;
+                }
+        }
+        }
+        if(check){
+            System.out.println(number+" is prime");
+        }else {
+            System.out.println(number+" not prime");
+        }
+        }
+    }
+
+
---
+++ src/test.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+
+        String name = "huynh";
+
+        System.out.printf("value is %32.12f", 32.33434);
+
+    }
+}
